1171816417f825b5eb0db7b640b9992b61ee846c
Daniel Stutz <stutzda1@students.zhaw.ch>
2011-11-17 13:09:30 +0100

    Neue Klassen

--- src/main/java/school2/dynProgramming/reflection/Address.java
+++ src/main/java/school2/dynProgramming/reflection/Address.java
@@ -4,4 +4,22 @@ public class Address {
 	private String street;
 	private int postcode;
 	private String city;
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public int getPostcode() {
+		return postcode;
+	}
+	public void setPostcode(int postcode) {
+		this.postcode = postcode;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
 }
--- src/main/java/school2/dynProgramming/reflection/Person.java
+++ src/main/java/school2/dynProgramming/reflection/Person.java
@@ -7,5 +7,23 @@ public class Person {
 	private String firstname;
 	private String lastname;
 	private ArrayList<Address> addressList=new ArrayList<Address>();
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public ArrayList<Address> getAddressList() {
+		return addressList;
+	}
+	public void setAddressList(ArrayList<Address> addressList) {
+		this.addressList = addressList;
+	}
 	
 }
